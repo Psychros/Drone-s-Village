746a50e5342d1b81596f7944a7e5d07e89bf27b9
lord349123 <t-ho@web.de>
2016-05-29 17:01:50 +0200

    The player can move the camera
    The moveSpeed is depending on the zoom

---
+++ Assets/Own Ressources/Scripts/CameraController.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+    //Camera
+    public float zoomSpeed = 1f;
+    public float minZoom = 10f,
+                 maxZoom = 60f;
+    public float moveSpeed = 1f,
+                 currentMoveSpeed;
+
+
+    void Start()
+    {
+        calculateCurrentMoveSpeed();
+    }
+
+    void Update () {
+        //Zoom the camera
+        if (Input.GetAxis("Mouse ScrollWheel") > 0)      //Zoom in
+            zoom(true);     
+        else if(Input.GetAxis("Mouse ScrollWheel") < 0)  //Zoom out
+            zoom(false);
+
+        //Move the camera
+        float moveX = Input.GetAxis("Mouse X");
+        float moveZ = Input.GetAxis("Mouse Y");
+        Camera.main.transform.position += new Vector3(moveX * currentMoveSpeed, 0, moveZ * currentMoveSpeed);
+    }
+
+
+
+    public void zoom(bool zoomIn)
+    {
+        if (zoomIn)
+        {
+            //Zoom in
+            Camera.main.fieldOfView -= zoomSpeed;
+            if (Camera.main.fieldOfView < minZoom)
+                Camera.main.fieldOfView = minZoom;
+        }
+        else
+        {
+            //Zoom out
+            Camera.main.fieldOfView += zoomSpeed;
+            if (Camera.main.fieldOfView > maxZoom)
+                Camera.main.fieldOfView = maxZoom;
+        }
+
+        calculateCurrentMoveSpeed();
+    }
+
+
+    private void calculateCurrentMoveSpeed()
+    {
+        currentMoveSpeed = moveSpeed / maxZoom * Camera.main.fieldOfView;
+    }
+}
